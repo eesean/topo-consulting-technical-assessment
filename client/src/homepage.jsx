7d154f5b0c2fa5b9f1f392aa83e9ab891ea8315a
Tan Ee Sean <tan.eesean@gmail.com>
2025-03-08 22:42:45 +0800

    fix app setup

---
+++ client/src/homepage.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Heading, Button, Container, Text } from '@chakra-ui/react';
+
+const Homepage = () => {
+    const [data, setData] = useState([]);
+
+    // Fetch data from Flask API
+    useEffect(() => {
+        axios.get("http://127.0.0.1:5000/api/data")
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching data:", error);
+            });
+    }, []);
+
+    return (
+        <>
+            <Heading my={10} fontSize="3xl" color="teal.500">
+                Home Page
+            </Heading>
+            <Container>
+                <Button
+                    m="5px"
+                    colorScheme="teal"
+                    width="480px"
+                    variant="solid"
+                    onClick={() => console.log("Choosing a language")}>
+                    Please choose a Language to learn!!
+                </Button>
+
+                {/* Display fetched data */}
+                <Heading fontSize="xl" mt={5}>Data from Flask:</Heading>
+                {data.length > 0 ? (
+                    data.map((item, index) => (
+                        <Text key={index}>{JSON.stringify(item)}</Text>
+                    ))
+                ) : (
+                    <Text>Loading data...</Text>
+                )}
+            </Container>
+        </>
+    );
+};
+
+export default Homepage;
